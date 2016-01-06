71892f20a836939480f98c8fb88562fded9c9041
unknown <mugywara117@gmail.com>
2016-01-06 23:45:03 +0100

    test

--- Reception.java
+++ Reception.java
@@ -6,24 +6,49 @@ import java.io.InputStream;
 public class Reception implements Runnable {
 
 	private InputStream in;
-	private String message = null;
+	private int message;
+	private byte[] DepositBuffer;
 	
 	public Reception(InputStream in){
-		
 		this.in = in;
 	}
 	
 	public void run() {
 		
-		while(true){/*
+		while(true){
 	        try {
-	        	
-			message = in.read(arg0);
+			message = in.read();
+			System.out.println(message);
 		    } catch (IOException e) {
-				
 				e.printStackTrace();
-			}*/
+			}
 		}
 	}
+	
+	
+	public void createDepositBuffer(byte[] buffer)
+	{
+	    DepositBuffer=buffer.clone();
+	}
 
+	public void addToDepositBuffer(byte[] buffer)
+	{
+		byte[] tmp= new byte[DepositBuffer.length+buffer.length];
+		for(int i=0;i<DepositBuffer.length;i++){
+			tmp[i]=DepositBuffer[i];
+		}
+		for(int i=0;i<buffer.length;i++){
+			tmp[i+DepositBuffer.length]=buffer[i];
+		}
+		DepositBuffer=tmp;
+	}
+	
+	
+	
+	
+	
 }
+	
+
+
+
