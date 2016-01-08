26be4663be261395a2ad9959f4900b4e93acef8c
unknown <mugywara117@gmail.com>
2016-01-08 16:38:46 +0100

    Modif idmsg

--- messages/IdentificationMessage.java
+++ messages/IdentificationMessage.java
@@ -2,10 +2,17 @@ package messages;
 
 import main.Message;
 
-public class IdentificationMessage implements Runnable {
+public class IdentificationMessage extends MessageToSend implements Runnable {
 	public static final int id = 4;
+	public String Account;
+	public String Password;
 	
-	public IdentificationMessage(Message msg) {
+	public IdentificationMessage(Message msg,String Account,String Password) {
+		super();
+		this.Account=Account;
+		this.Password=Password;
+		
+		
 		// create response message
 		run();
 	}
